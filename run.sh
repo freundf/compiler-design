f2c288bff0d63b8590dff6399e9ccde4c4d64c2d
freundf <fabio@freund.io>
2025-05-12 16:58:32 +0200

    added gcc to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # This script is supposed to run your compiler
-cabal run l1c -- $@
+input="$1"
+output="$2"
+
+cabal run l1c -- "$input" temp.s
+gcc temp.s -o "$output"
+
+rm temp.s
