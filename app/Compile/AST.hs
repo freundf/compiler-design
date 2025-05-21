1e179b36f6412c23d497dd59bb077feb1b1fc814
freundf <fabio@freund.io>
2025-05-21 11:55:56 +0200

    Extend AST for L2

--- app/Compile/AST.hs
+++ app/Compile/AST.hs
@@ -10,66 +10,115 @@ module Compile.AST
 import Data.List (intercalate)
 import Text.Megaparsec
 
-data AST =
-  Block [Stmt] SourcePos
+data AST = Program Block
+  deriving (Eq, Show)
+
+data Block = Block [Stmt] SourcePos
 
 data Stmt
-  = Decl String SourcePos
-  | Init String Expr SourcePos
+  -- Simple Statement
+  = Decl Type String SourcePos
+  | Init Type String Expr SourcePos
   | Asgn String AsgnOp Expr SourcePos
   | Ret Expr SourcePos
+  -- Control Statement
+  | While Expr Stmt SourcePos
+  | For (Maybe Stmt) Expr (Maybe Stmt) Stmt SourcePos
+  | If Expr Stmt (Maybe Stmt) SourcePos
+  | Break SourcePos
+  | Continue SourcePos
+  -- Block Statement
+  | InnerBlock Block
+  deriving (Eq, Show)
+  
+data Type = TInt | TBool
+  deriving (Eq, Show)
 
 data Expr
-  = IntExpr String SourcePos
+  = BoolLit Bool SourcePos
+  | IntExpr String SourcePos
   | Ident String SourcePos
-  | UnExpr Op Expr
-  | BinExpr Op Expr Expr
-
+  | UnExpr UnOp Expr
+  | BinExpr BinOp Expr Expr
+  | Ternary Expr Expr Expr
+  deriving (Eq, Show)
+  
 -- Nothing means =, Just is for +=, %=, ...
-type AsgnOp = Maybe Op
+type AsgnOp = Maybe BinOp
 
-data Op
-  = Mul
-  | Add
-  | Sub
-  | Div
-  | Neg
-  | Mod
+data BinOp
+  = Mul | Add | Sub | Div | Mod
+  | Lt | Leq | Gt | Geq
+  | Eq | Neq
+  | And | Or
+  | BitAnd | BitOr | BitXor
+  | Shl | Shr
+  
+data UnOp
+  = Neg | Not | BitNot
 
 -- re-exported for convenience
 posPretty :: SourcePos -> String
 posPretty = sourcePosPretty
 
 -- Some very basic pretty printing
-instance Show AST where
+instance Show Block where
   show (Block stmts _) =
     "Block: {\n" ++ intercalate "\n" (map show stmts) ++ "\n}"
 
 instance Show Stmt where
-  show (Decl name _) = "Decl: " ++ name
-  show (Init name e _) = "Init: " ++ name ++ " = " ++ show e
-  show (Asgn name op e _) =
-    "Assign: " ++ name ++ " " ++ show' op ++ " " ++ show e
-    where
-      show' (Just o) = show o ++ "="
-      show' Nothing = "="
-  show (Ret e _) = "Return: " ++ show e
+  show (Decl ty name _) = "Decl: " ++ show ty ++ " " ++ name
+    show (Init ty name e _) = "Init: " ++ show ty ++ " " ++ name ++ " = " ++ show e
+    show (Asgn name op e _) =
+      "Assign: " ++ name ++ showAsgnOp op ++ show e
+    show (Ret e _) = "Return: " ++ show e
+    show (If cond thn els _) =
+      "If (" ++ show cond ++ ") " ++ show thn ++ maybe "" ((" Else " ++) . show) els
+    show (While cond body _) =
+      "While (" ++ show cond ++ ") " ++ show body
+    show (For mInit cond mStep body _) =
+      "For (" ++ showOpt mInit ++ "; " ++ show cond ++ "; " ++ showOpt mStep ++ ") " ++ show body
+      where
+        showOpt (Just s) = show s
+        showOpt Nothing  = ""
+    show (Break _) = "Break"
+    show (Continue _) = "Continue"
+    show (InnerBlock blk) = show blk
 
 instance Show Expr where
+  show (BoolLit True _) = "true"
+  show (BoolLit False _) = "false"
   show (IntExpr i _) = i
   show (Ident name _) = name
   show (UnExpr op e) = "(" ++ show op ++ " " ++ show e ++ ")"
-  show (BinExpr op lhs rhs) =
-    "(" ++ show lhs ++ " " ++ show op ++ " " ++ show rhs ++ ")"
+  show (BinExpr op lhs rhs) = "(" ++ show lhs ++ " " ++ show op ++ " " ++ show rhs ++ ")"
+  show (Ternary cond t f) = "(" ++ show cond ++ " ? " ++ show t ++ " : " ++ show f ++ ")"
 
-instance Show Op where
-  show Mul = "*"
-  show Add = "+"
-  show Sub = "-"
-  show Div = "/"
-  show Neg = "-"
-  show Mod = "%"
-
+instance Show BinOp where
+  show Mul    = "*"
+  show Add    = "+"
+  show Sub    = "-"
+  show Div    = "/"
+  show Mod    = "%"
+  show Lt     = "<"
+  show Leq    = "<="
+  show Gt     = ">"
+  show Geq    = ">="
+  show Eq     = "=="
+  show Neq    = "!="
+  show And    = "&&"
+  show Or     = "||"
+  show BitAnd = "&"
+  show BitOr  = "|"
+  show BitXor = "^"
+  show Shl    = "<<"
+  show Shr    = ">>"
+  
+instance Show UnOp where
+  show Neg    = "-"
+  show Not    = "!"
+  show BitNot = "~"
+  
 showAsgnOp :: AsgnOp -> String
 showAsgnOp (Just op) = " " ++ show op ++ "= "
 showAsgnOp _ = " = "
